2ef43d88cf80e9ce0a79dfb7179e07a4dee788df
Wasller Souza <61431580+WasllerSouza@users.noreply.github.com>
2026-07-10 18:36:16 -0300

    feat: adicionar configuração de tradução do PrimeNG e ajustar estilos da interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  ConsultaLotesPageComponent
-} from './feature/outros-creditos-debitos/pages/consulta-lotes/consulta-lotes.component';
+import { ConsultaLotesPageComponent } from './feature/outros-creditos-debitos/pages/consulta-lotes/consulta-lotes.component';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ConsultaLotesPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'outros-creditos-debitos';
+  readonly config: PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+    this.config.setTranslation({
+      startsWith: 'Começa com',
+      contains: 'Contém',
+      notContains: 'Não contém',
+      endsWith: 'Termina com',
+      equals: 'Igual',
+      notEquals: 'Diferente',
+      noFilter: 'Sem Filtro',
+      lt: 'Menor que',
+      lte: 'Menor ou igual a',
+      gt: 'Maior que',
+      gte: 'Maior ou igual a',
+      is: 'É',
+      isNot: 'Não é',
+      before: 'Antes',
+      after: 'Depois',
+      dateAfter: 'Data depois de',
+      dateBefore: 'Data antes de',
+      dateIsNot: 'Data não é',
+      dateIs: 'Data é',
+      clear: 'Limpar',
+      apply: 'Aplicar',
+      matchAll: 'Corresponder Todos',
+      matchAny: 'Corresponder Qualquer',
+      addRule: 'Adicionar Regra',
+      removeRule: 'Remover Regra',
+      accept: 'Sim',
+      reject: 'Não',
+      choose: 'Escolher',
+      upload: 'Upload',
+      cancel: 'Cancelar',
+      dayNames: [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-feira',
+        'Sexta-feira',
+        'Sábado',
+      ],
+      dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+      dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+      monthNames: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+      ],
+      monthNamesShort: [
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
+      ],
+      today: 'Hoje',
+      weekHeader: 'Sem',
+    });
+  }
 }
