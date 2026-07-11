810f2c49f560cc857e97fbbe418f6ec3ffb64615
Wasller Souza <61431580+WasllerSouza@users.noreply.github.com>
2026-07-11 14:46:02 -0300

    feat: adicionar enum LISTA_CONTA_CORRENTE com dados de contas correntes

---
+++ src/app/shared/enums/conta-corrente.enum.ts
@@ -0,0 +1,15 @@
+import { IContaCorrente } from '../interfaces/conta-corrente.interface';
+
+export const LISTA_CONTA_CORRENTE: IContaCorrente[] = [
+  {
+    numero: '12345-6',
+    titular: 'Maria Aparecida da Silva',
+    cooperativa: '0001',
+  },
+  { numero: '23456-7', titular: 'João Carlos de Souza', cooperativa: '0002' },
+  {
+    numero: '34567-8',
+    titular: 'Cooperativa Vale Verde',
+    cooperativa: '0003',
+  },
+];
