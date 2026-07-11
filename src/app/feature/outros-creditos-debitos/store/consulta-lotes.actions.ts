1018f83648991714b86f2f395bba74d65c26a5d2
Wasller Souza <61431580+WasllerSouza@users.noreply.github.com>
2026-07-11 14:05:00 -0300

    feat: adicionar ações para gerenciar resultados de pesquisa em 'Consulta Lotes'

--- src/app/feature/outros-creditos-debitos/store/consulta-lotes.actions.ts
+++ src/app/feature/outros-creditos-debitos/store/consulta-lotes.actions.ts
@@ -6,6 +6,8 @@ export const ConsultaLotesActions = createActionGroup({
   source: 'Consulta Lotes',
   events: {
     Pesquisar: props<{ filtro: FiltroPesquisa }>(),
+    'Pesquisa Concluida': emptyProps(),
+    'Pesquisa Falhou': props<{ erro: string }>(),
     'Limpar Filtros': emptyProps(),
     'Selecionar Lotes': props<{ lotes: Lote[] }>(),
     'Trocar Pagina': props<{ pagina: number }>(),
