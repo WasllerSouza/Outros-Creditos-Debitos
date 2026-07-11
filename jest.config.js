7c65827bdceca66f9cf93afa1afc9870c56986dc
Wasller Souza <61431580+WasllerSouza@users.noreply.github.com>
2026-07-10 23:17:39 -0300

    chore: update Jest configuration for improved coverage reporting and module mapping

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,32 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'html', 'js'],
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "html", "js"],
   transform: {
-    '^.+\\.(ts|html)$': [
-      'jest-preset-angular',
-      { tsconfig: '<rootDir>/tsconfig.spec.json', stringifyContentPathRegex: '\\.html$' },
+    "^.+\\.(ts|mjs|js|html)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.html$",
+      },
     ],
   },
+  moduleNameMapper: {
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "\\.(css|less|scss|sass)$": "jest-transform-stub", // Add this line
+  },
   collectCoverageFrom: [
-    'src/app/**/*.ts',
-    '!src/app/**/*.spec.ts',
-    '!src/app/**/*.models.ts',
-    '!src/app/app.config.ts',
-    '!src/app/app.routes.ts',
+    "src/app/**/*.ts",
+    "src/**/*.ts",
+    "!src/main.ts",
+    "!src/polyfills.ts",
+    "!src/**/*.module.ts",
+    "!src/app/**/*.spec.ts",
+    "!src/app/**/*.models.ts",
+    "!src/app/app.config.ts",
+    "!src/app/app.routes.ts",
   ],
   coverageThreshold: {
     global: {
